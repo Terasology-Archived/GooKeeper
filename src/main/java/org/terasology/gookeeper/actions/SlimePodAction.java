65e173ad73cb150a73d2c1c7b37a50fd9f8cc625
l0ftyWhizZ <shreyasiyer14@gmail.com>
2018-05-14 09:10:44 +0530

    Started implementing slime pods.

---
+++ src/main/java/org/terasology/gookeeper/actions/SlimePodAction.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2017 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.gookeeper.actions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.terasology.engine.Time;
+import org.terasology.entitySystem.entity.EntityBuilder;
+import org.terasology.entitySystem.entity.EntityManager;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterMode;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
+import org.terasology.gookeeper.component.GooeyComponent;
+import org.terasology.gookeeper.component.PlazMasterComponent;
+import org.terasology.gookeeper.component.SlimePodComponent;
+import org.terasology.gookeeper.event.OnStunnedEvent;
+import org.terasology.gookeeper.input.DecreaseFrequencyButton;
+import org.terasology.gookeeper.input.IncreaseFrequencyButton;
+import org.terasology.logic.common.ActivateEvent;
+import org.terasology.logic.health.DoDamageEvent;
+import org.terasology.logic.location.LocationComponent;
+import org.terasology.logic.players.LocalPlayer;
+import org.terasology.math.TeraMath;
+import org.terasology.math.geom.Vector3f;
+import org.terasology.math.geom.Vector3i;
+import org.terasology.network.ClientComponent;
+import org.terasology.physics.CollisionGroup;
+import org.terasology.physics.HitResult;
+import org.terasology.physics.Physics;
+import org.terasology.physics.StandardCollisionGroup;
+import org.terasology.registry.In;
+import org.terasology.utilities.random.FastRandom;
+import org.terasology.utilities.random.Random;
+import org.terasology.world.BlockEntityRegistry;
+import org.terasology.world.WorldProvider;
+import org.terasology.world.block.Block;
+
+@RegisterSystem(RegisterMode.AUTHORITY)
+public class SlimePodAction extends BaseComponentSystem implements UpdateSubscriberSystem {
+    @In
+    private WorldProvider worldProvider;
+
+    @In
+    private Physics physicsRenderer;
+
+    @In
+    private BlockEntityRegistry blockEntityRegistry;
+
+    @In
+    private EntityManager entityManager;
+
+    @In
+    private Time time;
+
+    @In
+    private LocalPlayer localPlayer;
+
+    @Override
+    public void initialise() {
+    }
+
+    @Override
+    public void shutdown() {
+    }
+
+    @Override
+    public void update(float delta) {
+    }
+
+    @ReceiveEvent
+    public void onActivate(ActivateEvent event, EntityRef entity, SlimePodComponent slimePodComponent) {
+        slimePodComponent.isActivated = !slimePodComponent.isActivated;
+    }
+}
---
+++ src/main/java/org/terasology/gookeeper/component/SlimePodComponent.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.gookeeper.component;
+
+import org.terasology.entitySystem.Component;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.world.block.ForceBlockActive;
+
+public class SlimePodComponent implements Component {
+    /**
+     * The entity captured in the slime pod
+     */
+    public EntityRef capturedEntity = EntityRef.NULL;
+
+    /**
+     * The type of slime pod (since different types of gooeys require different types of slime pods)
+     */
+    public String podType;
+
+    /**
+     * The boolean whether the slime pod has been activated by the player or not
+     */
+    public boolean isActivated = false;
+}
