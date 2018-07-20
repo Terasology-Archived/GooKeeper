f7448624fb82928c7260b5dc608a7d345cee204e
Shreyas Iyer <Shreyas.Srinivasan@iiitb.org>
2018-07-20 08:30:53 +0530

    Added BreedingBlockComponent

--- src/main/java/org/terasology/gookeeper/system/BreedingSystem.java
+++ src/main/java/org/terasology/gookeeper/system/BreedingSystem.java
@@ -18,24 +18,37 @@ package org.terasology.gookeeper.system;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.assets.management.AssetManager;
+import org.terasology.behaviors.components.FollowComponent;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.gookeeper.component.BreedingBlockComponent;
 import org.terasology.gookeeper.component.GooeyComponent;
 import org.terasology.gookeeper.component.MatingComponent;
 import org.terasology.gookeeper.event.BreedGooeyEvent;
+import org.terasology.gookeeper.event.OnCapturedEvent;
+import org.terasology.logic.characters.events.OnEnterBlockEvent;
+import org.terasology.logic.location.LocationComponent;
 import org.terasology.logic.players.LocalPlayer;
+import org.terasology.math.geom.Vector3f;
+import org.terasology.math.geom.Vector3i;
 import org.terasology.registry.In;
 import org.terasology.registry.Share;
 import org.terasology.rendering.nui.NUIManager;
 import org.terasology.utilities.random.FastRandom;
 import org.terasology.utilities.random.Random;
+import org.terasology.world.BlockEntityRegistry;
+
+import java.math.RoundingMode;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
 @Share(value = BreedingSystem.class)
 public class BreedingSystem extends BaseComponentSystem {
+
+    @In
+    private BlockEntityRegistry blockEntityRegistry;
 
     @In
     private LocalPlayer localPlayer;
@@ -48,6 +61,35 @@ public class BreedingSystem extends BaseComponentSystem {
 
     private static final Logger logger = LoggerFactory.getLogger(BreedingSystem.class);
     private Random random = new FastRandom();
+
+    /**
+     * This method is called upon when a gooey that is following the player is brought to
+     * the love shack and it steps over a breeding block.
+     *
+     * Receives OnEnterBlockEvent when a gooey entity steps over a breeding block.
+     *
+     * @param event     The OnEnterBlockEvent
+     * @param entity    the gooey entity
+     */
+    @ReceiveEvent(components = {GooeyComponent.class})
+    public void onEnterBlock(OnEnterBlockEvent event, EntityRef entity) {
+        LocationComponent loc = entity.getComponent(LocationComponent.class);
+        Vector3f pos = loc.getWorldPosition();
+        pos.setY(pos.getY() -1);
+
+        EntityRef blockEntity = blockEntityRegistry.getExistingBlockEntityAt(new Vector3i(pos, RoundingMode.HALF_UP));
+
+        if (blockEntity.hasComponent(BreedingBlockComponent.class)) {
+            FollowComponent followComponent = entity.getComponent(FollowComponent.class);
+            BreedingBlockComponent breedingBlockComponent = blockEntity.getComponent(BreedingBlockComponent.class);
+
+            if (followComponent.entityToFollow != EntityRef.NULL && breedingBlockComponent.parentGooey == EntityRef.NULL) {
+                breedingBlockComponent.parentGooey = entity;
+                entity.send(new BreedGooeyEvent(followComponent.entityToFollow, entity));
+                blockEntity.saveComponent(breedingBlockComponent);
+            }
+        }
+    }
 
     /**
      * Receives the BreedGooeyEvent when the "activated" gooey is chosen for breeding.
