756fd26dee02958daad6f0383118fa8400cf94c4
Michael Pollind <mpollind@gmail.com>
2021-03-14 19:55:23 -0700

    update packages for inventory

--- src/main/java/org/terasology/gookeeper/system/SlimePodSystem.java
+++ src/main/java/org/terasology/gookeeper/system/SlimePodSystem.java
@@ -35,7 +35,7 @@ import org.terasology.engine.logic.characters.events.OnEnterBlockEvent;
 import org.terasology.engine.logic.common.ActivateEvent;
 import org.terasology.engine.logic.delay.DelayManager;
 import org.terasology.logic.health.HealthComponent;
-import org.terasology.logic.inventory.InventoryManager;
+import org.terasology.module.inventory.systems.InventoryManager;
 import org.terasology.engine.logic.inventory.PickupComponent;
 import org.terasology.engine.logic.inventory.events.DropItemEvent;
 import org.terasology.engine.logic.location.LocationComponent;
