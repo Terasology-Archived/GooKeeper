daae279bd1fca3bc9544772c8486e48bc0734dcf
Michael Pollind <mpollind@gmail.com>
2021-04-03 08:52:23 -0700

    chore: migrate gestalt v7
     PR: https://github.com/MovingBlocks/Terasology/pull/4593

--- src/main/java/org/terasology/gookeeper/system/BreedingSystem.java
+++ src/main/java/org/terasology/gookeeper/system/BreedingSystem.java
@@ -7,7 +7,7 @@ import org.joml.Vector3f;
 import org.joml.Vector3i;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.assets.management.AssetManager;
+import org.terasology.gestalt.assets.management.AssetManager;
 import org.terasology.behaviors.components.FollowComponent;
 import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.entitySystem.entity.EntityBuilder;
