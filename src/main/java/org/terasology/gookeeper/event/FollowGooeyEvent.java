f20df895f31154590b5138f62cf413561de7ecce
darkweird <foxflameless@gmail.com>
2021-09-07 17:55:26 +0300

    feature(gestaltv7-events): replace TS's Event to gestaltv7's Event

--- src/main/java/org/terasology/gookeeper/event/FollowGooeyEvent.java
+++ src/main/java/org/terasology/gookeeper/event/FollowGooeyEvent.java
@@ -16,7 +16,7 @@
 package org.terasology.gookeeper.event;
 
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.engine.entitySystem.event.Event;
+import org.terasology.gestalt.entitysystem.event.Event;
 
 public class FollowGooeyEvent implements Event {
 
