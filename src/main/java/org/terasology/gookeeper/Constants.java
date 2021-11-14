d3d1454e1f4cd81231cd7f4449f940712c6dacd2
ujjwa mishra <49057926+ujjman@users.noreply.github.com>
2021-11-14 12:20:02 +0530

    checkstyle: fix HideUtilityClassConstructor error without suppressing it
    this PR fixes  HideUtilityClassConstructor error without suppressing it

--- src/main/java/org/terasology/gookeeper/Constants.java
+++ src/main/java/org/terasology/gookeeper/Constants.java
@@ -2,12 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.gookeeper;
 
-@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
-public class Constants {
+public final class Constants {
     public static final String VISITOR_SPAWN_DELAY_EVENT_ID = "VISITOR_SPAWN_DELAY";
     public static final String DESTROY_ARROW_EVENT_ID = "DESTROY_ARROW";
     public static final String HEALTH_DECREASE_EVENT_ID = "DECREASE_HEALTH_TICK";
     public static final String GOOEY_DEATH_EVENT_ID = "DESTROY_GOOEY_ENTITY";
     public static final String SPAWN_GOOEY_EGG_EVENT_ID = "SPAWN_GOOEY_EGG";
     public static final String HATCH_EGG_EVENT_ID = "HATCH_GOOEY_EGG";
+    private Constants() {
+    }
 }
