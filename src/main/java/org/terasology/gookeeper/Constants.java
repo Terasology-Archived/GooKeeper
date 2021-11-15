fe4be2b43617a2b3fb44b300dbe25984ce37ba7d
ujjwal mishra <49057926+ujjman@users.noreply.github.com>
2021-11-16 01:46:52 +0530

    Update src/main/java/org/terasology/gookeeper/Constants.java
    Co-authored-by: jdrueckert <jd.rueckert@googlemail.com>

--- src/main/java/org/terasology/gookeeper/Constants.java
+++ src/main/java/org/terasology/gookeeper/Constants.java
@@ -9,6 +9,7 @@ public final class Constants {
     public static final String GOOEY_DEATH_EVENT_ID = "DESTROY_GOOEY_ENTITY";
     public static final String SPAWN_GOOEY_EGG_EVENT_ID = "SPAWN_GOOEY_EGG";
     public static final String HATCH_EGG_EVENT_ID = "HATCH_GOOEY_EGG";
+
     private Constants() {
     }
 }
