3678569850a3da5b0cc42641887a7cb2d5b3e0a1
Tobias Nett <skaldarnar@googlemail.com>
2022-01-13 23:04:24 +0100

    refactor: Adjust for Behaviors#88
    Adjust for refactorings done in Terasology/Behaviors#88.
    
    The changes resulted from automatic refactorings in IntelliJ and are **untested**.
    The IDE support only helps with Java classes, so there might be assets or other references that slipped through.
    
    At least Omega compiles with these changes.

--- src/main/java/org/terasology/gookeeper/actions/SetTargetToVisitBlockAction.java
+++ src/main/java/org/terasology/gookeeper/actions/SetTargetToVisitBlockAction.java
@@ -12,7 +12,7 @@ import org.terasology.engine.logic.location.LocationComponent;
 import org.terasology.engine.registry.In;
 import org.terasology.gookeeper.component.VisitBlockComponent;
 import org.terasology.gookeeper.component.VisitorComponent;
-import org.terasology.minion.move.MinionMoveComponent;
+import org.terasology.module.behaviors.components.MinionMoveComponent;
 import org.terasology.pathfinding.componentSystem.PathfinderSystem;
 
 import java.util.Random;
--- src/main/java/org/terasology/gookeeper/system/SlimePodSystem.java
+++ src/main/java/org/terasology/gookeeper/system/SlimePodSystem.java
@@ -6,7 +6,7 @@ import org.joml.Vector3f;
 import org.joml.Vector3i;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.behaviors.components.FollowComponent;
+import org.terasology.module.behaviors.components.FollowComponent;
 import org.terasology.engine.core.Time;
 import org.terasology.engine.entitySystem.entity.EntityBuilder;
 import org.terasology.engine.entitySystem.entity.EntityManager;
